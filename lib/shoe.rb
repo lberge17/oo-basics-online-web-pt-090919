9328e5800c8be3e9325a763ce126ac518bf3f96a
Laura Berge <lauraeberge@gmail.com>
2019-09-10 18:03:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,15 @@
 class Shoe
-  attr_accessor :brand
+  attr_accessor :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
   end
   
   def brand
     @brand
+  end
+  
+  def cobble
+    puts "The shoe is repaired."
+    @condition = "new"
   end
 end
